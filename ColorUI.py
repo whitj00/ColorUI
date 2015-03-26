da1cef82ca683851b893a1865a1c65588427ee8f
kuro112 <kuro@xtcr.net>
2015-03-26 11:42:26 -0700

    Last Touchups
    The project is pretty much finished, just a bug due to my lack of
    understanding of QT, once i fix the 'page' style display itll take a
    matter of mins to actually implemnt the send and issue asset methods

--- ColorUI.py
+++ ColorUI.py
@@ -9,11 +9,15 @@ def getBalance():
     global r
     r = requests.post(url + "getbalance").json()
 
-def sendAsset(address,amnt):
+def sendAsset(address,amnt,toaddy):
     global r2
-    payload = {'address': address, 'amount': amnt}
+    payload = {'address': address, 'to': toaddy, 'amount': amnt}
     r2 = requests.post(url + "sendasset", params=payload)
 
+def issueAsset(address,amnt,asset):
+    global r3
+    payload = {'address': address, 'asset': asset, 'amount': amnt}
+    r3 = requests.post(url + "issueasset", params=payload)
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -36,59 +40,64 @@ class Ui_Form(QtGui.QWidget):
 
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(488, 432)
+        Form.resize(427, 338)
         self.gridLayout = QtGui.QGridLayout(Form)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.treeWidget_2 = QtGui.QTreeWidget(Form)
-        self.treeWidget_2.setObjectName(_fromUtf8("treeWidget_2"))
-        self.gridLayout.addWidget(self.treeWidget_2, 0, 0, 1, 2)
-        self.treeWidget = QtGui.QTreeWidget(Form)
-        self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
-        self.gridLayout.addWidget(self.treeWidget, 1, 0, 1, 2)
-        self.label_2 = QtGui.QLabel(Form)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
-        self.label_3 = QtGui.QLabel(Form)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.gridLayout.addWidget(self.label_3, 2, 1, 1, 1)
-        self.lineEdit_2 = QtGui.QLineEdit(Form)
-        self.lineEdit_2.setText(_fromUtf8(""))
-        self.lineEdit_2.setObjectName(_fromUtf8("lineEdit_2"))
-        self.gridLayout.addWidget(self.lineEdit_2, 3, 0, 1, 1)
-        self.lineEdit_4 = QtGui.QLineEdit(Form)
-        self.lineEdit_4.setText(_fromUtf8(""))
-        self.lineEdit_4.setObjectName(_fromUtf8("lineEdit_4"))
-        self.gridLayout.addWidget(self.lineEdit_4, 3, 1, 1, 1)
         self.label = QtGui.QLabel(Form)
         self.label.setObjectName(_fromUtf8("label"))
-        self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
+        self.gridLayout.addWidget(self.label, 10, 0, 1, 1)
+        self.issue_amnt = QtGui.QLineEdit(Form)
+        self.issue_amnt.setText(_fromUtf8(""))
+        self.issue_amnt.setObjectName(_fromUtf8("issue_amnt"))
+        self.gridLayout.addWidget(self.issue_amnt, 9, 2, 1, 1)
+        self.addressWidget = QtGui.QTreeWidget(Form)
+        self.addressWidget.setObjectName(_fromUtf8("addressWidget"))
+        self.gridLayout.addWidget(self.addressWidget, 0, 0, 1, 3)
+        self.label_2 = QtGui.QLabel(Form)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 7, 0, 1, 1)
+        self.send_add = QtGui.QLineEdit(Form)
+        self.send_add.setText(_fromUtf8(""))
+        self.send_add.setObjectName(_fromUtf8("send_add"))
+        self.gridLayout.addWidget(self.send_add, 11, 0, 1, 1)
+        self.label_3 = QtGui.QLabel(Form)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.gridLayout.addWidget(self.label_3, 7, 2, 1, 1)
+        self.sa_btn = QtGui.QPushButton(Form)
+        self.sa_btn.setObjectName(_fromUtf8("sa_btn"))
+        self.gridLayout.addWidget(self.sa_btn, 13, 0, 1, 1)
+        self.ia_btn = QtGui.QPushButton(Form)
+        self.ia_btn.setObjectName(_fromUtf8("ia_btn"))
+        self.gridLayout.addWidget(self.ia_btn, 13, 2, 1, 1)
+        self.assetsWidget = QtGui.QTreeWidget(Form)
+        self.assetsWidget.setObjectName(_fromUtf8("assetsWidget"))
+        self.gridLayout.addWidget(self.assetsWidget, 2, 0, 1, 3)
         self.label_4 = QtGui.QLabel(Form)
         self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.gridLayout.addWidget(self.label_4, 4, 1, 1, 1)
-        self.lineEdit = QtGui.QLineEdit(Form)
-        self.lineEdit.setText(_fromUtf8(""))
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
-        self.gridLayout.addWidget(self.lineEdit, 5, 0, 1, 1)
-        self.lineEdit_3 = QtGui.QLineEdit(Form)
-        self.lineEdit_3.setText(_fromUtf8(""))
-        self.lineEdit_3.setObjectName(_fromUtf8("lineEdit_3"))
-        self.gridLayout.addWidget(self.lineEdit_3, 5, 1, 1, 1)
-        self.pushButton = QtGui.QPushButton(Form)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.gridLayout.addWidget(self.pushButton, 6, 0, 1, 1)
-        self.pushButton_2 = QtGui.QPushButton(Form)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.gridLayout.addWidget(self.pushButton_2, 6, 1, 1, 1)
+        self.gridLayout.addWidget(self.label_4, 10, 2, 1, 1)
+        self.issue_add = QtGui.QLineEdit(Form)
+        self.issue_add.setText(_fromUtf8(""))
+        self.issue_add.setObjectName(_fromUtf8("issue_add"))
+        self.gridLayout.addWidget(self.issue_add, 11, 2, 1, 1)
+        self.snd_amnt = QtGui.QLineEdit(Form)
+        self.snd_amnt.setText(_fromUtf8(""))
+        self.snd_amnt.setObjectName(_fromUtf8("snd_amnt"))
+        self.gridLayout.addWidget(self.snd_amnt, 9, 0, 1, 1)
+        self.rf_btn = QtGui.QPushButton(Form)
+        self.rf_btn.setObjectName(_fromUtf8("rf_btn"))
+        self.gridLayout.addWidget(self.rf_btn, 3, 2, 1, 1)
+        self.chw_btn = QtGui.QPushButton(Form)
+        self.chw_btn.setObjectName(_fromUtf8("chw_btn"))
+        self.gridLayout.addWidget(self.chw_btn, 3, 0, 1, 1)
 
         self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)   
 
     def retranslateUi(self, Form):
-        getBalance()
         Form.setWindowTitle(_translate("Form", "ColorUI", None))
-        self.treeWidget_2.headerItem().setText(0, _translate("Form", "Addresses", None))
-        __sortingEnabled = self.treeWidget_2.isSortingEnabled()
-        self.treeWidget_2.setSortingEnabled(False)
+        self.addressWidget.headerItem().setText(0, _translate("Form", "Addresses", None))
+        __sortingEnabled = self.addressWidget.isSortingEnabled()
+        self.addressWidget.setSortingEnabled(False)
 
         for item in r:
             qi = QtGui.QTreeWidgetItem()
@@ -96,35 +105,96 @@ class Ui_Form(QtGui.QWidget):
             qi_c = QtGui.QTreeWidgetItem()
             qi_c.setText(0, _translate("Form", item['value'], None))
             qi.addChild(qi_c)
-            self.treeWidget_2.addTopLevelItem(qi)
+            self.addressWidget.addTopLevelItem(qi)
 
-        self.treeWidget_2.setSortingEnabled(__sortingEnabled)
-        self.treeWidget.headerItem().setText(0, _translate("Form", "Assets", None))
-        __sortingEnabled = self.treeWidget.isSortingEnabled()
-        self.treeWidget.setSortingEnabled(False)
+        self.addressWidget.setSortingEnabled(__sortingEnabled)
+        self.assetsWidget.headerItem().setText(0, _translate("Form", "Assets", None))
+        __sortingEnabled = self.assetsWidget.isSortingEnabled()
+        self.assetsWidget.setSortingEnabled(False)
 
         for item in r:
+            print item
             for a in item['assets']:
                 qi = QtGui.QTreeWidgetItem()
                 qi.setText(0, _translate("Form", a['asset_id'], None))
                 qi_c = QtGui.QTreeWidgetItem()
                 qi_c.setText(0, _translate("Form", a['quantity'], None))
                 qi.addChild(qi_c)
-                self.treeWidget.addTopLevelItem(qi)
+                self.assetsWidget.addTopLevelItem(qi)
 
-        self.treeWidget.setSortingEnabled(__sortingEnabled)
-        self.label_2.setText(_translate("Form", "Amount:", None))
-        self.label_3.setText(_translate("Form", "Amount:", None))
-        self.label.setText(_translate("Form", "Address:", None))
+        self.assetsWidget.setSortingEnabled(__sortingEnabled)
+        self.label_2.setText(_translate("Form", "Ammount:", None))
+        self.label_3.setText(_translate("Form", "Ammount:", None))
+        self.sa_btn.setText(_translate("Form", "Send Assets", None))
+        self.ia_btn.setText(_translate("Form", "Issue Assets", None))
+        self.assetsWidget.headerItem().setText(0, _translate("Form", "Assets", None))
+        __sortingEnabled = self.assetsWidget.isSortingEnabled()
+        self.assetsWidget.setSortingEnabled(False)
+        self.assetsWidget.setSortingEnabled(__sortingEnabled)
         self.label_4.setText(_translate("Form", "Address:", None))
-        self.pushButton.setText(_translate("Form", "Send Assets", None))
-        self.pushButton_2.setText(_translate("Form", "Issue Assets", None))
+        self.rf_btn.setText(_translate("Form", "Refresh", None))
+        self.chw_btn.setText(_translate("Form", "Change Wallet", None))
+        self.rf_btn.clicked.connect(self.refresh)
+        self.chw_btn.clicked.connect(self.chWallet)
 
+    def refresh(self):
+        getBalance()
+
+    def chWallet(self):
+        app = QtGui.QApplication(sys.argv)
+        ex = Ui_Form2()
+        ex.show()
+        app.exec_()
+
+class Ui_Form2(QtGui.QWidget):
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.setupUi2(self)
+    def setupUi2(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(528, 227)
+        self.label = QtGui.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(190, -10, 141, 101))
+        self.label.setObjectName(_fromUtf8("label"))
+        self.lineEdit = QtGui.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(150, 140, 231, 16))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.label_2 = QtGui.QLabel(Form)
+        self.label_2.setGeometry(QtCore.QRect(230, 110, 51, 16))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.pushButton = QtGui.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(220, 180, 81, 23))
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.retranslateUi2(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi2(self, Form):
+        Form.setWindowTitle(_translate("Form", "Form", None))
+        self.label.setText(_translate("Form", "<html><head/><body><p><span style=\" font-size:28pt; font-weight:600;\">ColorUI</span></p></body></html>", None))
+        self.label_2.setText(_translate("Form", "Enter URL:", None))
+        self.pushButton.setText(_translate("Form", "Access Wallet", None))
+        self.pushButton.clicked.connect(self.screen2)
+
+
+
+    def screen2(form):
+        getBalance()
+        app = QtGui.QApplication(sys.argv)
+        ex = Ui_Form()
+        ex.show()
+        ex2.hide()
+        app.exec_()
 
 if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
-    ex = Ui_Form()
-    ex.show()
-    sys.exit(app.exec_())
+    getBalance()
+    app2 = QtGui.QApplication(sys.argv)
+    ex2 = Ui_Form2()
+    ex2.show()
+    sys.exit(app2.exec_())
+
+
+
+
+
 
 
